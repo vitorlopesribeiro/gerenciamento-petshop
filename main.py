d51e7bb04501701cbc42228510f9cbe50921dc71
vitorlopesribeiro <vitor.lopes.ribeiro@outlook.com>
2025-07-25 21:47:31 -0300

    alteração classe Cliente, Pet e menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from models.cliente import Cliente
+from models.pet import Pet
 
 clients = []
 
@@ -8,7 +9,6 @@ def menu():
         print("1 - Cadastrar cliente")
         print("2 - Adicionar pet ao cliente")
         print("3 - Listar clientes e seus pets")
-        print("4 - Criar serviço")
         print("5 - Sair")
         
         op = input("Escolha a opção: ")
@@ -17,11 +17,25 @@ def menu():
             print("Cadastro de cliente")
             nome = input("Digite seu nome: ").title().strip()
             cpf = input("Digite seu CPF: ")
-            novo_cliente = Cliente(nome, cpf)  
-            clients.append(novo_cliente.to_dict())
-            print(clients)
+            novo_cliente = Cliente(nome, cpf)
+            clients.append(novo_cliente)
             print(f"{novo_cliente} \nCadastrado!")
 
+        elif op == "2":
+            print("Adicionar pet ao cliente")
+            nome_pet = input("Digite o nome do Pet: ").title().strip()
+            idade_pet = input("Digite a idade do Pet: ")
+            novo_pet = Pet(nome_pet, idade_pet)
+            print(F"{novo_pet} \nCadastrado!")
+            cpf_pet = input("Digite o CPF do dono do Pet: ").title().strip()
+            for i,v in enumerate(clients):
+                if clients[i].get_cpf() == cpf_pet:
+                    clients[i].adicionar_pet(novo_pet)
+                    print(f"Pet: {nome_pet} - Cadastrado ao usuario: {clients[i]}")
+
+        elif op == "3":
+            for index,value in enumerate(clients):
+                print(f"Usuario {index}:", value)
         
         elif op == "5":
             print("Programa encerrado")
--- models/cliente.py
+++ models/cliente.py
@@ -4,22 +4,16 @@ class Cliente:
         self.__nome = nome
         self.__cpf = cpf
         self.__pets = []
-
+        
     def adicionar_pet(self, pet):
         self.__pets.append(pet)
 
     def listar_pets(self):
         return [str(p) for p in self.__pets]
-    
+
+    def get_cpf(self):
+        return self.__cpf 
+
     def __str__(self):
-        return f"Cliente: {self.__nome} - CPF: {self.__cpf}"
-    
-    def __len__(self):
-        return len(self.__pets)
-    
-    def to_dict(self):
-        return{
-            "nome":self.__nome,
-            "cpf":self.__cpf,
-        }
+        return f"Cliente: {self.__nome} - CPF: {self.__cpf} - Pets: {self.__pets}"
         
