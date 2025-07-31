1c4260cfe2f020b2dbce0eeb48b4488b047cf1e5
vitorlopesribeiro <vitor.lopes.ribeiro@outlook.com>
2025-07-31 15:10:10 -0300

    atualização menu

--- main.py
+++ main.py
@@ -1,57 +1,22 @@
-from models.cliente import Cliente
-from models.pet import Pet
-from models.servico import Servico
-
-clients = []
-
-def menu():
+def menu ():
     while True:
         print("\n=== MENU PRINCIPAL ===")
         print("1 - Cadastrar cliente")
         print("2 - Adicionar pet ao cliente")
         print("3 - Listar clientes e seus pets")
-        print("4 - Realizar Serviço")
-        print("5 - Sair")
-        
+        print("4 - Realizar atendimento")
+        print("5 - Listar histórico de atendimentos de um pet")
+        print("6 - Sair")
+
         op = input("Escolha a opção: ")
 
-        if op == "1":
-            print("Cadastro de cliente")
-            nome = input("Digite seu nome: ").title().strip()
-            cpf = input("Digite seu CPF: ")
-            novo_cliente = Cliente(nome, cpf)
-            clients.append(novo_cliente)
-            print(f"{novo_cliente} \nCadastrado!")
-
-        elif op == "2":
-            print("Adicionar pet ao cliente")
-            nome_pet = input("Digite o nome do Pet: ").title().strip()
-            idade_pet = input("Digite a idade do Pet: ")
-            novo_pet = Pet(nome_pet, idade_pet)
-            cpf_pet = input("Digite o CPF do dono do Pet: ").title().strip()
-            for i,v in enumerate(clients):
-                if clients[i].get_cpf() == cpf_pet:
-                    clients[i].adicionar_pet(novo_pet)
-                    print(f"Pet: {nome_pet}")
-                    print(f"Cadastrado ao usuario: {clients[i].get_name()}")
-                    print(f"Cadastrado com sucesso")
-
-        elif op == "3":
-            for index,value in enumerate(clients):
-                print(f"Usuario {index}:", value)
-
-        elif op == "4":
-            print("Realizar serviço")
-            tipo = input("Escolha o tipo de serviço").title()
-            valor = float(input("Digite o valor R$:"))
-            novo_servico = Servico(tipo,valor)
-            print(novo_servico)
-        
-        elif op == "5":
-            print("Programa encerrado")
+        if op == "6":
+            print("Saindo do sistema.")
             break
 
         else:
-            print("Opção invalida")
+            print("Você escolheu a opção", op)
 
 menu()
+
+    
